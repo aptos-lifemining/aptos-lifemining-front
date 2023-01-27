c48a5f09f97e4465d370b08d51128e8ae5d826e3
jdev08 <jdevv08@gmail.com>
2023-01-28 00:38:19 +0900

    feat: 헤더 및 상단 스토리 모음 1차 제작

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import Logo from '../public/svg/lifemining_logo.svg';
+
+export default function Header() {
+  return (
+    <React.Fragment>
+      <HeaderContainer>
+        <LogoIcon />
+      </HeaderContainer>
+    </React.Fragment>
+  );
+}
+
+const HeaderContainer = styled.div`
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  background: #ffffff;
+`;
+
+const LogoIcon = styled(Logo)`
+  width: 136px;
+  height: 40px;
+  margin: 0 24px 0;
+`;
---
+++ components/Stories.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import Image from 'next/image';
+
+export default function Stories() {
+  return (
+    <StoriesContainer>
+      <div className="swiper-container">
+        <StoryBubble>
+          <div className="avatar">
+            <Image src={'/img/sample.png'} width={58} height={58} alt="" />
+          </div>
+          <div className="user-nickname">#putdownfkjasldfddf</div>
+        </StoryBubble>
+
+        <StoryBubble>
+          <div className="avatar">
+            <Image src={'/img/sample.png'} width={58} height={58} alt="" />
+          </div>
+          <div className="user-nickname">#putdownfkjasldfddf</div>
+        </StoryBubble>
+
+        <StoryBubble>
+          <div className="avatar">
+            <Image src={'/img/sample.png'} width={58} height={58} alt="" />
+          </div>
+          <div className="user-nickname">#putdownfkjasldfddf</div>
+        </StoryBubble>
+
+        <StoryBubble>
+          <div className="avatar">
+            <Image src={'/img/sample.png'} width={58} height={58} alt="" />
+          </div>
+          <div className="user-nickname">#putdownfkjasldfddf</div>
+        </StoryBubble>
+
+        <StoryBubble>
+          <div className="avatar">
+            <Image src={'/img/sample.png'} width={58} height={58} alt="" />
+          </div>
+          <div className="user-nickname">#putdownfkjasldfddf</div>
+        </StoryBubble>
+      </div>
+    </StoriesContainer>
+  );
+}
+
+const StoriesContainer = styled.div`
+  height: 99px;
+  background: #ffffff;
+  border-bottom: 0.1px solid #333333;
+
+  .swiper-container::-webkit-scrollbar {
+    display: none;
+    width: 0;
+  }
+
+  .swiper-container {
+    display: flex;
+    align-items: center;
+    padding: 5px 26px 0 20px;
+    overflow: auto;
+    scrollbar-width: none;
+    position: relative;
+  }
+
+  .swiper-item {
+    width: 64px !important;
+    margin-right: 20px !important;
+    height: 82px;
+  }
+
+  .circle-box {
+    width: 63px;
+    height: 63px;
+    border-radius: 50%;
+    background-color: green;
+  }
+`;
+
+const StoryBubble = styled.div`
+  width: 64px !important;
+  margin-right: 20px !important;
+  height: 82px;
+
+  .avatar {
+    width: 64px;
+    height: 64px;
+    position: relative;
+    border-radius: 50%;
+    padding: 4px;
+    background-image: linear-gradient(to right top, rgba(86, 174, 255, 1), rgba(0, 26, 255, 1));
+
+    :before {
+      content: '';
+      position: absolute;
+      left: 1.5px;
+      top: 1.5px;
+      right: 1.5px;
+      bottom: 1.5px;
+      background: #ffffff;
+      border-radius: 50%;
+      z-index: 1;
+    }
+
+    img {
+      position: absolute;
+      left: 3px;
+      top: 3px;
+      z-index: 2;
+      display: block;
+      width: 58px;
+      height: 58px;
+      border-radius: 50%;
+      object-fit: cover;
+      object-position: center;
+    }
+  }
+
+  .user-nickname {
+    width: 64px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    font-family: 'Inter Tight';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 9px;
+    line-height: 11px;
+    text-align: center;
+    margin-top: 7px;
+  }
+`;
