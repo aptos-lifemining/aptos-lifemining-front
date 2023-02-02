f26adaf37106d57b7b13aa1ccbdfeb16c5915137
jdev08 <jdevv08@gmail.com>
2023-02-03 00:28:48 +0900

    feat: 인증서 민팅 페이지 추가

---
+++ pages/life_mint.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState } from 'react';
+import { LifeMintDataProvider } from '../context/LiftMintContext';
+import LifeMintTemplate from '../template/LifeMint';
+import SelectNameTemplate from '../template/mint/SelectName';
+import SelectChallengeTemplate from '../template/mint/SelectChallenge';
+import SelectVideoTemplate from '../template/mint/SelectVideo';
+import VideoPlayTemplate from '../template/mint/VideoPlay';
+import SelectPriceTemplate from '../template/mint/SelectPrice';
+import MintCompleteTemplate from '../template/mint/MintComplete';
+
+const SELECT_CHALLENGE_STEP = 0;
+const SELECT_VIDEO_STEP = 1;
+const VIDEO_PLAY_STEP = 2;
+const SELECT_NAME_STEP = 3;
+const SELECT_PRICE_STEP = 4;
+const COMPLETE_STEP = 4;
+
+export const StepContext = createContext(null);
+
+export default function LifeMint() {
+  const [step, setStep] = useState(2);
+
+  return (
+    <StepContext.Provider value={{ step, setStep }}>
+      <LifeMintDataProvider>
+        {step === SELECT_CHALLENGE_STEP && <SelectChallengeTemplate />}
+        {step === SELECT_VIDEO_STEP && <SelectVideoTemplate />}
+        {step === VIDEO_PLAY_STEP && <VideoPlayTemplate />}
+        {step === SELECT_NAME_STEP && <SelectNameTemplate />}
+        {step === SELECT_PRICE_STEP && <MintCompleteTemplate />}
+      </LifeMintDataProvider>
+    </StepContext.Provider>
+  );
+}
